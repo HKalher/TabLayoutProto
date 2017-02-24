3c0e52ad44837d88da9008b0bd97e1c6272a6e57
HKalher <hkkalher@gmail.com>
2017-02-24 11:44:19 +0530

    adding files

---
+++ app/src/main/java/henu/kalher/com/tablayoutproto/MainActivity.java
@@ -0,0 +1,13 @@
+package henu.kalher.com.tablayoutproto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
